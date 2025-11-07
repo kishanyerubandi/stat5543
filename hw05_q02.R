e6dbffe163cef6b2b240179fa88912f769b7ffc6
Kishan Yerubandi <kishan.yerubandi@gmail.com>
2025-11-07 09:08:25 -0600

    Add files via upload

--- hw05_q02.R
+++ hw05_q02.R
@@ -5,9 +5,17 @@ my_df <- read.table(input_path, sep = ",", header=TRUE)
 my_df$NBHD <- trimws(my_df$NBHD) #Remove whitespace.
 my_df$NBHD <- factor(my_df$NBHD) #Convert to factor.
 
-my_model <- lm(SALES ~ LAND*factor(NBHD) + IMP*factor(NBHD), data = my_df)
-summary(my_model)
+my_model_A <- lm(SALES ~ LAND*factor(NBHD) + IMP*factor(NBHD), data = my_df)
+summary(my_model_A)
+
+#2c:
+my_model_B <- lm(SALES ~ LAND + IMP, data = my_df)
+anova(my_model_B, my_model_A)
 
 #2e:
 my_property <- data.frame(LAND=50,IMP=180,NBHD="HUNTERSGREEN")
-predict(my_model, my_property)
+predict(my_model_A, my_property)
+
+#2f:
+my_model_C <- lm(SALES ~ LAND + IMP*factor(NBHD), data = my_df)
+anova(my_model_C, my_model_A)
